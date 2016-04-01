e8889fd734b16a71ee6b1d1d1a37b4d10b744d55
Nguyen Dinh Nguyen <nguyendinhnguyen92@gmail.com>
2016-04-01 02:42:47 -0600

    some commit

--- ParallelPhoneAccService2/Program.cs
+++ ParallelPhoneAccService2/Program.cs
@@ -27,36 +27,55 @@ namespace ParallelPhoneAccService2
 
         // Set env to Debugging to display log messages
         // Set env to run the program at full speed
-        private const int env = (int)Env.Normal;
+        private const Env env = Env.Debugging;
+
+        // AutoResetEvent for signaling when to start processing IDlists
+        private static AutoResetEvent hasIDLists = new AutoResetEvent(false);
+
+        // AutoResetEvent for signaling when all accounts have been processed
+        private static AutoResetEvent isFinished = new AutoResetEvent(false);
 
         static void Main(string[] args)
         {
             List<PhoneAcc> accounts = new List<PhoneAcc>(); // List for storing the final accounts to be displayed
             Queue<IDList> idListQueue = new Queue<IDList>(); // FIFO queue of IDLists retrieved from the web service
-
             bool allListRetrieved = false;
-
-            // Create thread to download IDLists
-            Thread idListThread = new Thread(() => acquireIDLists(ref idListQueue, ref allListRetrieved));
 
             // Start timing
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
 
-            // Start thread to download IDLists
-            idListThread.Start();
+            #region processIDListsTask
 
-            // As long as there's IDLists left either in the web service or in the IDList queue
-            // Keep processing accounts
-            while (!allListRetrieved || idListQueue.Count > 0)
+            // Create task to process acquired IDLists
+            // Make the task wait on the hasIDLists event
+            Task processIDListsTask = Task.Run(() =>
             {
-                // If there are IDLists in the queue
-                if (idListQueue.Count > 0)
+                // As long as there's IDLists left in the IDList queue or in the web service database
+                // Keep processing IDLists
+                while (idListQueue.Count > 0 || !allListRetrieved)
                 {
+                    if (env == Env.Debugging)
+                    {
+                        Console.WriteLine("Thread {0}: aiting for IDLists to process", Thread.CurrentThread.ManagedThreadId);
+                    }
+
+                    // Wait on the hasIDLists event
+                    // Only start the task if the event is raised
+                    // This makes sure that the while loop is not a busy wait loop
+                    // However, only wait if there's still ID lists yet to be downloaded
+                    if (!allListRetrieved)
+                    {
+                        hasIDLists.WaitOne();
+                    }
+                  
+                    // Since this code section is only reached if the hasIDLists event is raised
+                    // it is not necessary to check if idListQueue.Count is larger than 0
+
                     // Get the oldest IDList retrieved
                     IDList list = idListQueue.Peek();
 
-                    // Process each ID in the IDList
+                    // Process each account in the IDList based on ID
                     Parallel.ForEach(list.result, (id) =>
                     {
                         string json = null;
@@ -79,7 +98,7 @@ namespace ParallelPhoneAccService2
 
                         PhoneAcc acc = JsonConvert.DeserializeObject<PhoneAcc>(json);
 
-                        if (env == (int)Env.Debugging)
+                        if (env == Env.Debugging)
                         {
                             Console.WriteLine("Thread {0}: retrieved account: {1}", Thread.CurrentThread.ManagedThreadId, acc.ToString());
                         }
@@ -99,41 +118,111 @@ namespace ParallelPhoneAccService2
                                     where results.hasValidNumber()
                                     orderby results.age
                                     select results).Take(numRes).ToList();
-
                         // Since all the IDs in the list have been processed, pop the list out of the queue
                         idListQueue.Dequeue();
                     }
                 }
-            }
 
-            // Sort the results by name
-            accounts = accounts.OrderBy(account => account.name).ToList();
+                if (env == Env.Debugging)
+                {
+                    Console.WriteLine("Thread {0}: all accounts processed", Thread.CurrentThread.ManagedThreadId);
+                }
 
-            // Stop timing
-            stopWatch.Stop();
+                // Once all accounts have been processed
+                isFinished.Set();
+            });
 
-            long time = stopWatch.ElapsedMilliseconds;
+            #endregion            
 
-            // Print out the result
-            Console.WriteLine();
-            Console.WriteLine("Time elapsed: {0} ms", time);
-            Console.WriteLine(accounts.Count + " results sorted by name: ");
-            for (int i = 0; i < accounts.Count; i++)
+            #region downloadIDListsTask
+
+            // Create task to download IDLists
+            Task downloadIDListsTask = Task.Run(() =>
             {
-                Console.WriteLine(accounts[i].ToString());
-            }
+                // The first download has a null token
+                string token = null;
 
+                do
+                {
+                    IDList list = downloadIDList(token);
+
+                    // Update the token
+                    if (string.IsNullOrEmpty(list.token))
+                    {
+                        token = null;
+                    }
+                    else
+                    {
+                        token = list.token;
+                    }
+
+                    // Add the list to the FIFO list queue
+                    lock (thisLock)
+                    {
+                        idListQueue.Enqueue(list);
+                    }
+
+                    // Signal that there's at least 1 IDList in the IDList queue
+                    hasIDLists.Set();
+
+                    if (env == Env.Debugging)
+                    {
+                        Console.WriteLine("Thread {0}: downloaded list: {1}", Thread.CurrentThread.ManagedThreadId, list.ToString());
+                    }
+                }
+                while (!string.IsNullOrEmpty(token));
+
+                if (env == Env.Debugging)
+                {
+                    Console.WriteLine("Thread {0}: all IDLists downloaded", Thread.CurrentThread.ManagedThreadId);
+                }
+
+                // Once all IDLists have been retrieved
+                lock (thisLock)
+                {
+                    allListRetrieved = true;
+                }
+            });
+
+            #endregion
+
+            #region processResultTask
+
+            Task processResultTask = Task.Run(() => 
+            {
+                // Wait until the isFinished event is raised
+                isFinished.WaitOne();
+
+                // Sort the results by name
+                accounts = accounts.OrderBy(account => account.name).ToList();
+
+                // Stop timing
+                stopWatch.Stop();
+
+                long time = stopWatch.ElapsedMilliseconds;
+
+                // Print out the result
+                Console.WriteLine();
+                Console.WriteLine("Time elapsed: {0} ms", time);
+                Console.WriteLine(accounts.Count + " results sorted by name: ");
+                for (int i = 0; i < accounts.Count; i++)
+                {
+                    Console.WriteLine(accounts[i].ToString());
+                }
+            });
+
+            #endregion
 
             // Wait for return key to pause the console
             Console.ReadLine();
         }
 
         /// <summary>
-        /// Query the web service asynchronously to retrieve a single list of account IDs
+        /// Query the web service to retrieve a single list of account IDs
         /// </summary>
         /// <param name="token">Token for retrieving ID list</param>
         /// <returns>The retrieved ID list</returns>
-        private static IDList downloadIDListAsync(string token)
+        private static IDList downloadIDList(string token)
         {
             string json = null;
             IDList list = null;
@@ -150,7 +239,7 @@ namespace ParallelPhoneAccService2
                     string url = ENDPOINT_URL;
 
                     // If there's no token
-                    if (String.IsNullOrEmpty(token))
+                    if (string.IsNullOrEmpty(token))
                     {
                         url = url + LIST_ROUTE;
                     }
@@ -168,51 +257,6 @@ namespace ParallelPhoneAccService2
                 }
             }
             return list;
-        }
-
-        /// <summary>
-        /// As long as there are new tokens, download and add IDLists to the IDList queue
-        /// Once all the lists have been downloaded, mark allListRetrieved as true
-        /// </summary>
-        /// <param name="idListQueue">IDList queue for storing downloaded IDLists</param>
-        /// <param name="allListRetrieved">Boolean variable for indicating whether all lists have been downloaded</param>
-        private static void acquireIDLists(ref Queue<IDList> idListQueue, ref bool allListRetrieved)
-        {
-            // The first download has a null token
-            string token = null;
-
-            do
-            {
-                IDList list = downloadIDListAsync(token);
-
-                // Update the token
-                if (string.IsNullOrEmpty(list.token))
-                {
-                    token = null;
-                }
-                else
-                {
-                    token = list.token;
-                }
-
-                // Add the list to the FIFO list queue
-                lock (thisLock)
-                {
-                    idListQueue.Enqueue(list);
-                }
-
-                if (env == (int)Env.Debugging)
-                {
-                    Console.WriteLine("Thread {0}: downloaded list: {1}", Thread.CurrentThread.ManagedThreadId, list.ToString());
-                }
-            }
-            while (!string.IsNullOrEmpty(token));
-
-            // Mark that all lists have been downloaded
-            lock (thisLock)
-            {
-                allListRetrieved = true;
-            }
         }
     }
 }
